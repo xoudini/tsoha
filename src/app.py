e387ec0b99a6274c214d6bb9b90593fbc597c577
Dan Lindholm <main@xoudini.com>
2017-12-28 22:31:24 +0200

    Updated application.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,20 @@
 from flask import Flask
 
+from src.controllers.base import BaseController
+from src.utilities.database import DatabaseManager
+
+# Required instances.
 app = Flask(__name__)
+dbm = DatabaseManager(database="tsoha")
+
+# Testing database connection.
+@app.route("/connection")
+def connection():
+    return dbm.test_connection()
 
 @app.route("/")
-def home():
-	return "<p style=\"font-size: 15pt; font-family: Menlo, monospace;\">Up and running.<p>"
-
+def index():
+    return BaseController.index()
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
