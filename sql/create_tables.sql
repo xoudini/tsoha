a0b1145e5bfdf14d8e1a015eea8547a4daa0c89e
Dan Lindholm <main@xoudini.com>
2017-12-28 22:28:01 +0200

    Added SQL files.

---
+++ sql/create_tables.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS Account (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    admin BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS Profile (
+    account_id INTEGER REFERENCES Account(id),
+    display_name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Tag (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Thread (
+    id SERIAL PRIMARY KEY,
+    author_id INTEGER REFERENCES Profile(account_id),
+    title TEXT NOT NULL,
+    created TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Response (
+    id SERIAL PRIMARY KEY,
+    author_id INTEGER REFERENCES Profile(account_id),
+    thread_id INTEGER REFERENCES Thread(id),
+    content TEXT NOT NULL,
+    created TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ThreadTag (
+    tag_id INTEGER REFERENCES Tag(id),
+    thread_id INTEGER REFERENCES Thread(id),
+    UNIQUE(tag_id, thread_id)
+);
