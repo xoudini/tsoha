ec8a92c77099b197f96b6ed892fee35bc3d5d937
Dan Lindholm <main@xoudini.com>
2017-12-29 17:02:00 +0200

    Added some test data.

---
+++ sql/add_test_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO Account (username, password, admin) VALUES ('dan', '12345', TRUE);
+INSERT INTO Account (username, password, admin) VALUES ('test', '12345', FALSE);
+INSERT INTO Profile (account_id, display_name) VALUES (1, 'Dan L');
+INSERT INTO Profile (account_id, display_name) VALUES (2, 'Test User');
+INSERT INTO Tag (title) VALUES ('discussion');
+INSERT INTO Tag (title) VALUES ('meta');
+INSERT INTO Tag (title) VALUES ('suggestion');
+INSERT INTO Tag (title) VALUES ('programming');
+INSERT INTO Thread (author_id, title, created) VALUES (1, 'First post, ignore.', now() AT TIME ZONE 'utc');
+INSERT INTO Response (author_id, thread_id, content, created) VALUES (1, 1, 'Read the title.', now() AT TIME ZONE 'utc');
+INSERT INTO Response (author_id, thread_id, content, created) VALUES (2, 1, '<ignoring>', now() AT TIME ZONE 'utc');
+INSERT INTO Thread (author_id, title, created) VALUES (1, 'Add more tags.', now() AT TIME ZONE 'utc');
+INSERT INTO Response (author_id, thread_id, content, created) VALUES (1, 2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', now() AT TIME ZONE 'utc');
+INSERT INTO ThreadTag (tag_id, thread_id) VALUES (2, 2);
+INSERT INTO ThreadTag (tag_id, thread_id) VALUES (3, 2);
