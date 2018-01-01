5033267e061bfdb427f9e61e167bf5fbcd2c8a4e
Dan Lindholm <main@xoudini.com>
2018-01-01 21:56:12 +0200

    Fix type hints, modified match checking for convenience.

--- src/utilities/password.py
+++ src/utilities/password.py
@@ -10,11 +10,15 @@ class PasswordUtility:
     def encode_string(self, unencoded: str) -> bytes:
         validated = self.validate_string(unencoded)
         return bytes(validated, encoding='utf-8')
+    
+    def decode_string(self, encoded: bytes) -> str:
+        return encoded.decode('utf-8')
 
-    def generate_hash(self, password: str) -> str:
+    def generate_hash(self, password: str) -> bytes:
         encoded = self.encode_string(password)
         return bcrypt.hashpw(encoded, bcrypt.gensalt())
 
-    def matches(self, password: str, passwordhash: str) -> bool:
-        encoded = self.encode_string(password)
-        return bcrypt.checkpw(encoded, passwordhash)
+    def matches(self, password: str, hash_string: str) -> bool:
+        encoded_password = self.encode_string(password)
+        encoded_hash = bytes(hash_string, encoding='utf-8')
+        return bcrypt.checkpw(encoded_password, encoded_hash)
