71ab0eadedf521e8037f9a3e0c066fa691546154
Dan Lindholm <main@xoudini.com>
2018-01-02 22:51:06 +0200

    Tag model.

---
+++ src/models/tag.py
@@ -0,0 +1,61 @@
+from src.models.base import BaseModel
+from src.shared import db
+
+from typing import List
+
+class Tag(BaseModel):
+
+    def __init__(self, uid: int, title: str, count: int):
+        self.uid = uid
+        self.title = title
+        self.count = count
+    
+    @staticmethod
+    def all() -> List['Tag']:
+        rows = db.execute_query(
+            """
+            SELECT Tag.*, COUNT(ThreadTag.tag_id) as count FROM Tag
+            LEFT JOIN ThreadTag ON ThreadTag.tag_id = Tag.id
+            GROUP BY Tag.id
+            ORDER BY count DESC, title;
+            """
+        )
+
+        result = []
+
+        for row in rows:
+            tag = Tag(row['id'], row['title'], row['count'])
+            result.append(tag)
+        
+        return result
+
+    @staticmethod
+    def find_by_id(uid: int) -> 'Tag':
+        rows = db.execute_query(
+            # """
+            # SELECT Tag.*, COUNT(ThreadTag.tag_id) as count FROM Tag
+            # WHERE id = %(id)s
+            # LEFT JOIN ThreadTag ON ThreadTag.tag_id = Tag.id
+            # GROUP BY Tag.id;
+            # """,
+            """
+            SELECT *, (
+                SELECT COUNT(*) FROM ThreadTag
+                WHERE ThreadTag.tag_id = %(id)s
+            ) AS count FROM Tag
+            WHERE id = %(id)s;
+            """,
+            {'id': uid}
+        )
+
+        try:
+            row = rows.pop(0)
+            tag = Tag(row['id'], row['title'], row['count'])
+            return tag
+
+        except:
+            return None
+
+
+    def __repr__(self):
+        return "Tag - id: " + str(self.uid) + ", title: " + self.title
