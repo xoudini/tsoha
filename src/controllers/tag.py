beae8a17d1b0a960b189f526f92fe2b08f39be61
Dan Lindholm <main@xoudini.com>
2018-01-02 23:58:10 +0200

    Updated tag controller for creating tags.

--- src/controllers/tag.py
+++ src/controllers/tag.py
@@ -14,3 +14,11 @@ class TagController(BaseController):
     def view_for_tag(uid: int):
         tag = Tag.find_by_id(uid)
         return render_template('tag.html', title="Tag", tag=tag)
+
+    @staticmethod
+    def view_for_new_tag():
+        return render_template('newtag.html', title="New tag")
+
+    @staticmethod
+    def create_tag(title: str):
+        Tag.create(title)
