a2f6c46deffec228aa4cc0a2abc25fef9527d864
Dan Lindholm <main@xoudini.com>
2018-01-08 21:25:23 +0200

    Add model for account.

---
+++ src/models/account.py
@@ -0,0 +1,49 @@
+from src.models.base import BaseModel
+from src.shared import db, pw
+
+class Account(BaseModel):
+    
+    def __init__(self, uid: int, username: str, display_name: str = None, admin: bool = False):
+        self.uid = uid
+        self.username = username
+        # self.password_hash = password_hash
+        self.display_name = display_name
+        self.admin = admin
+
+    @staticmethod
+    def authenticate(username: str, password: str):
+        rows = db.execute_query(
+            """
+            SELECT * FROM Account WHERE username = %(username)s;
+            """,
+            {'username': username}
+        )
+
+        try:
+            row = rows.pop(0)
+            stored_hash = row['password']
+            if pw.matches(password, stored_hash):
+                account = Account(row['id'], row['username'], row['display_name'], row['admin'])
+                return {'user': account}
+        
+        except:
+            pass
+        
+        return {'error': "Incorrect username or password.", 'username': username}
+
+    @staticmethod
+    def find_by_id(uid: int) -> 'Account':
+        rows = db.execute_query(
+            """
+            SELECT id, username, display_name, admin FROM Account WHERE id = %(id)s;
+            """,
+            {'id': uid}
+        )
+
+        try:
+            row = rows.pop(0)
+            account = Account(row['id'], row['username'], row['display_name'], row['admin'])
+            return account
+
+        except:
+            return None
