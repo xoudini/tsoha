a8a63fcadad6849aba53d42a2995bf6ff9785ab5
Dan Lindholm <main@xoudini.com>
2018-01-08 21:34:20 +0200

    Sessions and sign in now working.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, session
 
 # Set up shared instances, once.
 import src.shared as shared
@@ -6,10 +6,26 @@ shared.setup()
 
 # Controllers.
 from src.controllers.hello import HelloWorldController # TODO: remove
+from src.controllers.account import AccountController
 from src.controllers.tag import TagController
 
 # Application instance.
 app = Flask(__name__)
+app.secret_key = shared.secret_key
+
+
+# Convenience methods for session.
+def get_user_id() -> int:
+    if 'user_id' in session:
+        return session['user_id']
+
+def get_username() -> str:
+    if 'username' in session:
+        return session['username']
+
+def get_display_name() -> str:
+    if 'display_name' in session:
+        return session['display_name']
 
 
 ### Testing database connection ###
@@ -79,13 +95,41 @@ def new_tag():
 
 ### Account endpoints ###
 
-@app.route("/signin")
+@app.route("/signin", methods=['GET', 'POST'])
 def signin():
-    return HelloWorldController.signin()
+    if request.method == 'GET':
+        return AccountController.view_for_signin()
+    else:
+        username, password = request.form['username'], request.form['password']
+        result = AccountController.signin(username, password)
+
+        if 'user' in result:
+            # TODO: Make account serializable.
+            account = result['user']
+            session['user_id'] = account.uid
+            session['username'] = account.username
+            session['display_name'] = account.display_name
+            session['admin'] = account.admin
+            session['fresh_signin'] = True
+            return redirect(url_for('profile'))
+        else:
+            return AccountController.view_for_signin(result)
+
+@app.route("/signout", methods=['POST'])
+def signout():
+    session.pop('user_id', None)
+    session.pop('display_name', None)
+    session.pop('admin', None)
+    return redirect(url_for('signin'))
 
 @app.route("/profile")
 def profile():
-    return HelloWorldController.profile()
+    if 'fresh_signin' in session:
+        name = get_display_name() if get_display_name() else get_username()
+        session.pop('fresh_signin')
+        return AccountController.view_for_profile(get_user_id(), {'welcome': "Welcome back " + name + "!"})
+    
+    return AccountController.view_for_profile(get_user_id())
 
 @app.route("/profile/edit")
 def editprofile():
