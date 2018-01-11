70f98a92e51aabca402d1da38c0fd4cacd1fe5d6
Dan Lindholm <main@xoudini.com>
2018-01-12 00:08:28 +0200

    Added required methods for creating threads in ThreadController.

--- src/controllers/thread.py
+++ src/controllers/thread.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from src.controllers.base import BaseController
 
+from typing import Dict, List
+
 from src.models.thread import Thread
 from src.models.tag import Tag
 
@@ -15,3 +17,13 @@ class ThreadController(BaseController):
     def view_for_thread(uid: int):
         thread = Thread.find_by_id(uid)
         return render_template('thread.html', title="Thread", thread=thread)
+
+    @staticmethod
+    def view_for_new_thread(messages: Dict[str, str] = None):
+        tags = Tag.all()
+        return render_template('new_thread.html', title="New thread", tags=tags, messages=messages)
+
+    @staticmethod
+    def create(user_id: int, title: str, content: str, tag_ids: List[int]):
+        result = Thread.create(user_id, title, content, tag_ids)
+        return result
