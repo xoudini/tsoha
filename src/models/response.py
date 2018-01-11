2e771cd8caed0cdc347baf4b85d0bbc28fb50a55
Dan Lindholm <main@xoudini.com>
2018-01-11 21:51:08 +0200

    Added model for responses.

---
+++ src/models/response.py
@@ -0,0 +1,40 @@
+from src.models.base import BaseModel
+from src.shared import db
+
+# Required models
+from src.models.account import Account
+
+from typing import List
+import datetime
+
+class Response(BaseModel):
+    
+    def __init__(self, uid: int, author: Account, content: str, created: datetime):
+        self.uid = uid
+        self.author = author
+        self.content = content
+        self.created = "{0:%Y/%m/%d %H:%M}".format(created)
+
+    @staticmethod
+    def find_by_thread_id(thread_id: int) -> List['Response']:
+        rows = db.execute_query(
+            """
+            SELECT 
+                Response.*,
+                Account.username, Account.display_name
+            FROM Response
+            INNER JOIN Account ON Account.id = Response.author_id
+            AND Response.thread_id = %(thread_id)s
+            ORDER BY created ASC;
+            """,
+            {'thread_id': thread_id}
+        )
+
+        result = []
+
+        for row in rows:
+            account = Account(row['author_id'], row['username'], row['display_name'])
+            response = Response(row['id'], account, row['content'], row['created'])
+            result.append(response)
+        
+        return result
